682d313f26c0b2cb89592548025efc421cf5fee6
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-19 00:24:27 +0100

    Started building up the first view :)

--- app/Site/admin/views/settings.blade.php
+++ app/Site/admin/views/settings.blade.php
@@ -1,1 +1,8 @@
-test
+{{ Form::open() }}
+
+    {{ Form::label('sitename', 'Site name: ') }}
+    {{ Form::text('sitename') }}
+    {{ Form::label('rooturl', 'Root url: ') }}
+    {{ Form::text('rooturl', Request::root()); }}
+
+{{ Form::close() }}
