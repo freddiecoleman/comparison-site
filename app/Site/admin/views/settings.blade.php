0b93be786b79a1bab95c58c601f8e947109b8d4e
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-19 20:01:46 +0100

    Building form

--- app/Site/admin/views/settings.blade.php
+++ app/Site/admin/views/settings.blade.php
@@ -2,7 +2,15 @@
 
     {{ Form::label('sitename', 'Site name: ') }}
     {{ Form::text('sitename') }}
+    {{ Form::label('sitetitle', 'Site title: ') }}
+    {{ Form::text('rooturl'); }}
     {{ Form::label('rooturl', 'Root url: ') }}
     {{ Form::text('rooturl', Request::root()); }}
+    {{ Form::label('username', 'Username: ') }}
+    {{ Form::text('username'); }}
+    {{ Form::label('password', 'Password: ') }}
+    {{ Form::password('password'); }}
+    {{ Form::label('confirm', 'Confirm: ') }}
+    {{ Form::password('confirm'); }}
 
 {{ Form::close() }}
