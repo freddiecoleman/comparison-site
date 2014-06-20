aaeea09943296b05f8d8950b12081c2346f4e629
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-20 09:01:06 +0100

    made initial db seeding check if data already exists to prevent duplication of settings

--- app/database/seeds/SettingsTableSeeder.php
+++ app/database/seeds/SettingsTableSeeder.php
@@ -4,17 +4,26 @@ class SettingsTableSeeder extends Seeder {
 
     public function run()
     {
-        Site\Admin\Models\Settings::create(
-            array('setting' => 'site_title', 'value' => 'Comparison Site')
-        );
+        $siteTitle = Site\Admin\Models\Settings::where('setting','=','site_title')->get()->first();
 
-        Site\Admin\Models\Settings::create(
-            array('setting' => 'site_title', 'value' => 'The worlds best site for comparing stuff!')
-        );
+        if (is_null($siteTitle)){
 
-        Site\Admin\Models\Settings::create(
-            array('setting' => 'root_url', 'value' => Request::root())
-        );
+            Site\Admin\Models\Settings::create(
+                array('setting' => 'site_title', 'value' => 'Comparison Site')
+            );
+
+        }
+
+        $rootUrl = Site\Admin\Models\Settings::where('setting','=','root_url')->get()->first();
+
+        if (is_null($rootUrl)){
+
+            Site\Admin\Models\Settings::create(
+                array('setting' => 'root_url', 'value' => Request::root())
+            );
+
+        }
+
     }
 
 }
