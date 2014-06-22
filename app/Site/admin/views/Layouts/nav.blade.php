6e19c31534754399373390f22c9b54690fedc070
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-22 22:30:19 +0100

    built in menu highlighting using route names

--- app/Site/admin/views/Layouts/nav.blade.php
+++ app/Site/admin/views/Layouts/nav.blade.php
@@ -11,10 +11,10 @@
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active">{{ link_to_route('siteConfig', 'Site Config') }}</li>
-            <li>{{ link_to_route('subjects', 'Subjects') }}</li>
-            <li>{{ link_to_route('types', 'Types') }}</li>
-            <li>{{ link_to_route('attributes', 'Attributes') }}</li>
+            <li{{ (Route::currentRouteName() ==  'siteConfig') ? ' class="active"' : '' }}>{{ link_to_route('siteConfig', 'Site Config') }}</li>
+            <li{{ (Route::currentRouteName() ==  'subjects') ? ' class="active"' : '' }}>{{ link_to_route('subjects', 'Subjects') }}</li>
+            <li{{ (Route::currentRouteName() ==  'types') ? ' class="active"' : '' }}>{{ link_to_route('types', 'Types') }}</li>
+            <li{{ (Route::currentRouteName() ==  'attributes') ? ' class="active"' : '' }}>{{ link_to_route('attributes', 'Attributes') }}</li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
