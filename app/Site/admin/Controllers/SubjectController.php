d9770c9b24bb82c76e44519e817ffa77928b687b
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-22 10:10:23 +0100

    start making it possible to edit subjects

--- app/Site/admin/Controllers/SubjectController.php
+++ app/Site/admin/Controllers/SubjectController.php
@@ -11,71 +11,16 @@ class SubjectController extends \BaseController {
         return \View::make('admin::subject')->with(array('subjects' => $subjects));
 	}
 
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
 		//
 	}
 
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
 		//
 	}
 
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
 		//
--- app/routes.php
+++ app/routes.php
@@ -7,5 +7,6 @@ Route::get('/', function()
 
 Route::get('/admin/settings', array('as' => 'siteConfig', 'uses' => 'Site\Admin\Controllers\SettingsController@index'));
 Route::get('/admin/subjects', array('as' => 'subjects', 'uses' => 'Site\Admin\Controllers\SubjectController@index'));
+Route::get('/admin/subject/{id}', array('as' => 'editSubject', 'uses' => 'Site\Admin\Controllers\SubjectController@edit'));
 Route::get('/admin/subject/types', array('as' => 'types', 'uses' => 'Site\Admin\Controllers\TypeController@index'));
 Route::get('/admin/subject/type/attributes', array('as' => 'attributes', 'uses' => 'Site\Admin\Controllers\AttributeController@index'));
