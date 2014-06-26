cb8fba67fc5e25984ab50fdabfcc91af04d9415c
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-26 22:49:33 +0100

    built alias for attributes to be used to reference columns in mongoDB

--- app/database/migrations/2014_06_21_200348_create_attributes_table.php
+++ app/database/migrations/2014_06_21_200348_create_attributes_table.php
@@ -16,6 +16,7 @@ class CreateAttributesTable extends Migration {
 		{
 			$table->increments('id');
             $table->string('name');
+            $table->string('alias');
 			$table->timestamps();
 		});
 	}
--- app/database/seeds/AttributesTableSeeder.php
+++ app/database/seeds/AttributesTableSeeder.php
@@ -9,19 +9,19 @@ class AttributesTableSeeder extends Seeder {
         if (is_null($attributes)){
 
             Site\Admin\Models\Attribute::create(
-                array('name' => 'Monthly cost')
+                array('name' => 'Monthly cost', 'alias' => 'monthlyCost')
             );
 
             Site\Admin\Models\Attribute::create(
-                array('name' => 'Annual cost')
+                array('name' => 'Annual cost', 'alias' => 'annualCost')
             );
 
             Site\Admin\Models\Attribute::create(
-                array('name' => 'Cost')
+                array('name' => 'Cost', 'alias' => 'cost')
             );
 
             Site\Admin\Models\Attribute::create(
-                array('name' => 'Quality score')
+                array('name' => 'Quality score', 'alias' => 'qualityScore')
             );
 
         }
